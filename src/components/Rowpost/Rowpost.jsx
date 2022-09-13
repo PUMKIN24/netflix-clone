c5e8c61af5d3831369cfbaebe51d135d4c4477d2
PUMKIN24 <jer2ymatheww@gmail.com>
2022-09-12 18:12:01 -0700

    Trailer view added

--- src/components/Rowpost/Rowpost.jsx
+++ src/components/Rowpost/Rowpost.jsx
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from 'react'
+import Youtube from 'react-youtube'
+import { API_KEY } from '../../constants/constants'
 import './Rowpost.css'
 import {imageUrl} from '../../constants/constants'
 import axios from '../../axios'
 
 const Rowpost = (props) => {
   const [movies, setMovies] = useState([])
+  const [urlId, setUrlId] = useState('')
   useEffect(() => {
     axios.get(props.url).then(response=>{
       console.log(response.data)
@@ -13,6 +16,24 @@ const Rowpost = (props) => {
       alert('Network Error')
     })
   }, [])
+
+  const opts = {
+    height: '390',
+    width: '100%',
+    playerVars: {
+      autoplay: 1,
+    }
+  }
+  const handleMovieTrailer = (id)=> {
+    console.log(id)
+    axios.get(`/movie/${id}/videos?api_key=${API_KEY}&language=en-US`).then(response=>{
+      if(response.data.results.length!==0){
+        setUrlId(response.data.results[0])
+      }else {
+        alert("Trailer Not Available")
+      }
+    })
+  }
   
   return (
     <div className='row'>
@@ -21,11 +42,12 @@ const Rowpost = (props) => {
           {movies.map((obj)=>
 
 
-              <img className={props.isSmall ? 'smallPoster' : 'poster'} src= {`${imageUrl+obj.backdrop_path}`} alt="poster" />
+              <img onClick={()=> handleMovieTrailer(obj.id)} className={props.isSmall ? 'smallPoster' : 'poster'} src= {`${imageUrl+obj.backdrop_path}`} alt="poster" />
           )}
            
             
         </div>
+         { urlId &&   <Youtube opts={opts} videoId={urlId.key}  />  }
     </div>
   )
 }
